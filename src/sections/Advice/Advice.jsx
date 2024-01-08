f98f9ad7ae5e3e2231de17567120b9fd25b2ec0c
saksham <sakshamjain0464@gmail.com>
2024-01-08 12:49:04 +0530

    Optimized Heo Section using QuoteCard Component

--- src/sections/Advice/Advice.jsx
+++ src/sections/Advice/Advice.jsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import QuoteCard from '../../components/QuoteCard/QuoteCard'
+
+export default function Hero() {
+  const [quote, setQuote] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    async function fetchQuote() {
+      const data = await fetch("https://api.quotable.io/quotes/random");
+      const jsonData = await data.json();
+      setQuote(jsonData[0]);
+      setTimeout(() => {
+        setLoaded(true);
+      }, 100);
+    }
+    fetchQuote();
+  }, []);
 
 
-export default function Advice() {
   return (
-    <div>
-      <h1>Advide</h1>
+    <div className="h-full w-full flex items-center justify-center">
+      <div className="h-full w-[90%] sm:max-w-[75%] sm:w-fit mt-5 sm:mt-20">
+        <div className="w-full flex items-center justify-center flex-col mb-10 text-center">
+          <h1 className="sm:text-5xl text-3xl mb-5 ">Welcome to SparkSoul</h1>
+          <p className="text-xl tracking-widest">
+            Your one stop destination for all quotes.
+          </p>
+        </div>
+        <div className="flex justify-center flex-col w-full">
+          <div className="text-gray-900">
+            <h1 className="text-3xl tracking-wider font-medium">
+              Today&apos;s Quote
+            </h1>
+          </div>
+          {loaded ? (
+            <QuoteCard Quote={quote}/>
+          ) : (
+            <div className="mt-5">Loading...</div>
+          )}
+        </div>
+        <div className="mt-8">
+          <Link
+            to="/quotes"
+            className="bg-gray-800 px-5 py-2 rounded-lg text-gray-300 hover:bg-gray-700 hover:text-white">
+            Get More Quotes <i className="fa-solid fa-arrow-right" />
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import QuoteCard from '../../components/QuoteCard/QuoteCard'
 
 export default function Hero() {
   const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-  const [copyTextDisplay, setCopyTextDisplay] = useState(false);
-  const [copied, setCopied] = useState(false);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchQuote() {
       const data = await fetch("https://api.quotable.io/quotes/random");
       const jsonData = await data.json();
-      setQuote(jsonData[0].content);
-      setAuthor(jsonData[0].author);
+      setQuote(jsonData[0]);
       setTimeout(() => {
         setLoaded(true);
       }, 100);
@@ -21,13 +18,6 @@ export default function Hero() {
     fetchQuote();
   }, []);
 
-  const copyQuote = () => {
-    window.navigator.clipboard.writeText(quote);
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 1000);
-  };
 
   return (
     <div className="h-full w-full flex items-center justify-center">
@@ -45,27 +35,7 @@ export default function Hero() {
             </h1>
           </div>
           {loaded ? (
-            <div className="w-fit min-w-[80%] shadow-md relative hover:shadow-xl transition-all duration-500 px-16 py-16 text-lg tracking-wider rounded-md justify-self-center">
-              <blockquote>
-                <q>{quote}</q>
-              </blockquote>
-              <p className="italic text-right mt-2 mr-[-40px]">~ {author}</p>
-              <div className="absolute top-5 right-8">
-                <i
-                  className="fa-regular fa-clipboard text-2xl cursor-pointer hover:text-gray-700 active:text-gray-500"
-                  onClick={copyQuote}
-                  onMouseEnter={() => setCopyTextDisplay(true)}
-                  onMouseLeave={() => setCopyTextDisplay(false)}></i>
-                <p
-                  className={`absolute text-xs font-semibold text-center top-[-25px] right-[-5px]  ${
-                    copyTextDisplay ? "block" : "hidden"
-                  } ${"hover:top-[-10px]"} ${
-                    copied ? "text-green-700" : "text-gray-900"
-                  }`}>
-                  {copied ? "Copied" : "Copy"}&#33;
-                </p>
-              </div>
-            </div>
+            <QuoteCard Quote={quote}/>
           ) : (
             <div className="mt-5">Loading...</div>
           )}
@@ -73,7 +43,7 @@ export default function Hero() {
         <div className="mt-8">
           <Link
             to="/quotes"
-            className="bg-gray-800 mt-8 px-5 py-2 rounded-lg text-gray-300 hover:bg-gray-700 hover:text-white">
+            className="bg-gray-800 px-5 py-2 rounded-lg text-gray-300 hover:bg-gray-700 hover:text-white">
             Get More Quotes <i className="fa-solid fa-arrow-right" />
           </Link>
         </div>
