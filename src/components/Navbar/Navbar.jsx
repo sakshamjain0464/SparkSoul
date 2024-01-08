06ed260e88f3952026c85c209aa31f593fd42aa1
saksham <sakshamjain0464@gmail.com>
2024-01-08 12:43:22 +0530

    Minor Bug Fixes!!

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
-                <h1 className="text-2xl text-white font-semibold">SparkSoul</h1>
+                <NavLink to={'/'}><h1 className="text-2xl text-white font-semibold">SparkSoul</h1></NavLink>
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
